cf04649e2c6e9866c2e70663d6a58430ab2d843b
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-17 13:31:38 +1100

    Testing

---
+++ main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  For loop
+//
+//  Created by Eun Jae Lee on 17/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+//  for(intitilized; condition; incre/decre) { statement,,,}
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    int i;
+    
+    for(i=1;i<=10;i++) {
+        
+        printf("%d \n",i);
+        
+    }
+    
+    return 0;
+}
